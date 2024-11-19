2b52ba2e761f11754b89ba172b516bb6e71c5bc9
btwbeatriz <beatriz.ffreitas@hotmail.com>
2024-11-19 17:31:03 -0300

    integraçao com aws s3

---
+++ src/main/java/com/rocketseat/createUrlShorter/UrlData.java
@@ -0,0 +1,14 @@
+package com.rocketseat.createUrlShorter;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class UrlData {
+
+    private String originalUrl;
+    private long expirationTime;
+}
